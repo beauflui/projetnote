3006ae72499c123d61b452f8261cba404f05a8eb
LUIDJY BEAUFOND (Etudiant) <beauflui@ur.univ-rouen.fr>
2023-03-13 11:27:29 +0100

    Version avec bootstrap

--- server.js
+++ server.js
@@ -3,11 +3,26 @@ let app = express();
 let port = 3000;
 
 app.use(express.static(__dirname + '/www'));
+app.use('/js',express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js',express.static(__dirname + '/node_modules/jquery/dist'));
+app.use ('/js',express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
+app.use('/css',express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+
+
 
 app.listen(port, ()=>{
     console.log ('Le serveur est en route');
     console.log (`serveur listening at http://localhost:${port}'`);
 })
+
+
+
+
 app.get('/',(req,res, next) =>{
     res.sendFile( '/www/index.html');
+});
+
+app.get('/accueil',(req,res, next) =>{
+    res.sendFile( '/www/accueil.html');
 });

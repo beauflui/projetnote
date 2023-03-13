3fd8dec9699fb5c5367870f899978f56fed254d3
LUIDJY BEAUFOND (Etudiant) <beauflui@ur.univ-rouen.fr>
2023-03-13 10:56:59 +0100

    Premier serveur node opérationel

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+let port = 3000;
+
+app.use(express.static(__dirname + '/www'));
+
+app.listen(port, ()=>{
+    console.log ('Le serveur est en route');
+    console.log (`serveur listening at http://localhost:${port}'`);
+})
+app.get('/',(req,res, next) =>{
+    res.sendFile( '/www/index.html');
+});
